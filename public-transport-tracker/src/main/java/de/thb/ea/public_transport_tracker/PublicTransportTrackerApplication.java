70e736b820d13250977bc8588f4802ac54760e19
EMX107 <46888679+EMX107@users.noreply.github.com>
2025-11-23 00:06:22 +0100

    Add initial spring boot project structure

---
+++ public-transport-tracker/src/main/java/de/thb/ea/public_transport_tracker/PublicTransportTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.thb.ea.public_transport_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicTransportTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicTransportTrackerApplication.class, args);
+	}
+
+}
