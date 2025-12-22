f824753dbb1d3826f4cf5a6b00179cd933caba67
EMX107 <46888679+EMX107@users.noreply.github.com>
2025-12-22 22:25:31 +0100

    style users site

--- public-transport-tracker-ng/src/app/user/user.component.ts
+++ public-transport-tracker-ng/src/app/user/user.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { UserService } from "./user.service";
 import { IUser } from "./model/user";
+import { RouterLink } from "@angular/router";
+import { DatePipe } from "@angular/common";
 
 @Component({
-    selector: 'app-user',
+    selector: 'app-users',
     standalone: true,
     providers: [UserService],
     templateUrl: './user.component.html',
-    styleUrl: './user.component.css'
+    styleUrl: './user.component.css',
+    imports: [
+        RouterLink,
+        DatePipe
+    ]
 })
 export class UserComponent implements OnInit {
 
