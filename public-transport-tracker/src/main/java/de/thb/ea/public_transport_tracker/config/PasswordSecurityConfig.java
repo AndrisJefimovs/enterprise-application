0f67e7bb781a8ee88b1a1a7dbd758a66525d665f
EMX107 <46888679+EMX107@users.noreply.github.com>
2025-12-24 14:32:38 +0100

    Clean up

--- public-transport-tracker/src/main/java/de/thb/ea/public_transport_tracker/config/PasswordSecurityConfig.java
+++ public-transport-tracker/src/main/java/de/thb/ea/public_transport_tracker/config/PasswordSecurityConfig.java
@@ -1,13 +1,9 @@
 package de.thb.ea.public_transport_tracker.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class PasswordSecurityConfig {
@@ -16,17 +12,5 @@ public class PasswordSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    UrlBasedCorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        
-        return source;
-    }
+       
 }
