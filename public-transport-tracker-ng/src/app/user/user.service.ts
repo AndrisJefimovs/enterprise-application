ccefc447d4134c21d89a2531e105fd5a5f1310a2
EMX107 <46888679+EMX107@users.noreply.github.com>
2026-01-04 14:48:01 +0100

    Rename api string to uppercase

--- public-transport-tracker-ng/src/app/user/user.service.ts
+++ public-transport-tracker-ng/src/app/user/user.service.ts
@@ -8,30 +8,30 @@ import { IUser } from "./model/user";
 })
 export class UserService {
 
-    private readonly api: string = "http://localhost:8080/api/v1/users"
+    private readonly API: string = "http://localhost:8080/api/v1/users"
 
     private users: IUser[] = []
 
     constructor(private httpClient: HttpClient) {};
 
     public getUsers(): Observable<IUser[]> {
-        return this.httpClient.get<IUser[]>(this.api);
+        return this.httpClient.get<IUser[]>(this.API);
     }
 
     public getUser(userId: number): Observable<IUser> {
-        return this.httpClient.get<IUser>(this.api + "/" + String(userId));
+        return this.httpClient.get<IUser>(this.API + "/" + String(userId));
     }
 
     public updateUser(user: IUser): Observable<HttpResponse<IUser>> {
-        return this.httpClient.put<IUser>(this.api + "/" + user.id, user, { observe: 'response' });
+        return this.httpClient.put<IUser>(this.API + "/" + user.id, user, { observe: 'response' });
     }
 
     public createUser(user: IUser): Observable<HttpResponse<IUser>> {
-        return this.httpClient.post<IUser>(this.api, user, { observe: 'response' });
+        return this.httpClient.post<IUser>(this.API, user, { observe: 'response' });
     }
 
     public deleteUser(userId: number): Observable<HttpResponse<IUser>> {
-        return this.httpClient.delete<IUser>(this.api + "/" + String(userId), { observe: 'response' });
+        return this.httpClient.delete<IUser>(this.API + "/" + String(userId), { observe: 'response' });
     }
 
 }
