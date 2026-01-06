c2ca789a1f3c3fada9ffe2fbc4cbc7140a998a02
EMX107 <46888679+EMX107@users.noreply.github.com>
2026-01-06 19:55:54 +0100

    Change config name

---
+++ public-transport-tracker/src/main/java/de/thb/ea/public_transport_tracker/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package de.thb.ea.public_transport_tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+@Configuration
+public class WebClientConfig {
+    
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+            .build();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+            .configure(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT, true)
+            .build();
+    }
+
+}
